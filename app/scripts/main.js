18bf61712b1ba6dca31c5cd07bda0898a222cacb
Jean-Philippe Bidegain <jp@bidega.in>
2012-12-28 09:46:40 +0100

    [+] Ajout de fonctions utilitaires et d'un loader

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,6 +4,29 @@ window.crateDigger = {
 	Collections: {},
 	Views: {},
 	Routers: {},
+	utils : {
+		vendor: function (a, f) {
+			for (var d = {}, c = ["-webkit-", "-moz-", "-ms-", "-o-"], h = 0; h < c.length; h++) d[c[h] + a] = f;
+			d[a] = f;
+			return d;
+		},
+		loading: function() {
+			(new Spinner()).spin(document.getElementById('loading'));
+			$('#loading').hide();
+		},
+		touch: function() {
+			$('a').on('touchstart', function () {
+				if (!$(this).hasClass('pressed')) {
+					$(this).addClass('pressed');
+				}
+			});
+			$('a').on('touchend', function () {
+				if ($(this).hasClass('pressed')) {
+					$(this).removeClass('pressed');
+				}
+			});
+		}
+	},
 	init: function() {
 		console.log('Hello from Backbone!');
 	}
@@ -11,6 +34,8 @@ window.crateDigger = {
 
 $(document).ready(function(){
 	crateDigger.init();
+	crateDigger.utils.loading();
+	crateDigger.utils.touch();
 	
 	router = new crateDigger.Routers.ApplicationRouter();
 	Backbone.history.start();

2c89649343de604431247c73af258cf457bd6850
Jean-Philippe Bidegain <jp@bidega.in>
2012-12-28 17:21:04 +0100

    [+] Project : Refactoring

--- app/scripts/views/release-view.js
+++ app/scripts/views/release-view.js
@@ -1,11 +1,11 @@
 crateDigger.Views.releaseView = Backbone.View.extend({
-	tagName: "article",
-    className: "release-container media",
-    template: $("#releaseTemplate").html(),
-    attributes: {'itemscope': '', 'itemtype': 'http://schema.org/MusicGroup'},
-    render: function () {
-        var tmpl = _.template(this.template);
-        this.$el.html(tmpl(this.model.toJSON()));
-        return this;
-    }
+	tagName: 'article',
+	className: 'release-container media',
+	template: $('#releaseTemplate').html(),
+	attributes: {'itemscope': '', 'itemtype': 'http://schema.org/MusicGroup'},
+	render: function () {
+		var tmpl = _.template(this.template);
+		this.$el.html(tmpl(this.model.toJSON()));
+		return this;
+	}
 });
